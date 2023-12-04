4674a1b5569b29f241d822600620afb1edc4c501
Arjun Ashok <125722020+for-trancer@users.noreply.github.com>
2023-12-04 18:36:51 +0530

    added 16bit user input

--- 8086/oddeven.asm
+++ 8086/oddeven.asm
@@ -1,27 +1,38 @@
-data segment 
-no db 0002h;
-str1 db "even$"
-str2 db "odd$"
+data segment
+	str1 db " is odd $"
+	str2 db " is even $"
+	str3 db "enter the number : $"
+
+	display macro msg
+		lea dx,msg
+		mov ah,09h
+		int 21h
+	endm
 data ends
 
 code segment
 assume cs:code,ds:data
-start: 
+start:
 	mov ax,data
 	mov ds,ax
-	mov ax,no
-	test ax,1
-	jz iseven
-	mov ah,09h
-	mov dx,offset str2
+
+	display str3
+	mov ah,01h
 	int 21h
+	mov ah,01h
+	int 21h
+
+	test al,1 ; perform the bitwise AND operation with 1 and checks the lsb
+	jnz is_odd ; if lsb not zero it will be odd
+	display str2 ; else it will be even 
+	jmp exit
+
+is_odd:
+	display str1
+
+exit:
 	mov ah,4ch
 	int 21h
-iseven:
-	mov ah,09h
-	mov dx,offset str1
-	int 21h
-	mov ah,4ch
-	int 21h
+
 code ends
-end start
+	end start
