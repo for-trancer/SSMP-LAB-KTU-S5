a247f7b2001ec591c5684c8b908f6cd81b07d8a3
Arjun Ashok <125722020+for-trancer@users.noreply.github.com>
2023-12-05 22:49:19 +0530

    added comments

--- 8086/search.asm
+++ 8086/search.asm
@@ -1,5 +1,5 @@
 data segment
-list db 1,2,3,4,5
+list db 1,2,3,4, ; array list
 msg db 'Enter The Number To Search : $'
 str1 db ' is found!$'
 str2 db ' is not found!$'
@@ -17,26 +17,25 @@ start:
 	mov ds,ax
 	
 	display msg
-	mov ah,01h
+	mov ah,01h ; taking input number to search for
 	int 21h
-	sub al,30h
-	mov cl,al
-	mov si,0
+	sub al,30h ; converting it to normal form from ascii format
+	mov cl,al ; moving it to cl for comparison
+	mov si,0 ; si -> index 0
 
 search_loop:
-	mov al,[list+si]
-	cmp al,cl
-	je is_found
+	mov al,[list+si] ; moving data from the list with si'th index to al (first element is at si=0 and second is at si=1...)
+	cmp al,cl ; comparing the data with out input number
+	je is_found ; je -> jump equal 
 	inc si
-	cmp si,5
-	jl search_loop
-not_found:
+	cmp si,5 ; comparing the index that it has traversed all the element in the array 
+	jl search_loop ; if not repeat the process
+not_found: ; this part will be executed after traversing the array and we got no matching data
 	display str2
-	mov ah,4ch
-	int 21h
+	jmp exit ; we dont want to display the is found part if it is not found so jumping
 is_found:
 	display str1
-	mov ah,4ch
+exit:mov ah,4ch
 	int 21h
 code ends
 end start
