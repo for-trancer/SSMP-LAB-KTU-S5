286d4f5f6c1a655a7341b4ff218295b56365d0ed
Arjun Ashok <125722020+for-trancer@users.noreply.github.com>
2023-12-07 17:22:25 +0530

    modified the code to not to display fractional part

--- 8086/average.asm
+++ 8086/average.asm
@@ -1,19 +1,18 @@
 data segment
-list db 01h,02h,03h,04h,05h
-msg db 'enter the number : $'
-str db 'average : $'
-str1 db ' . $'
-display macro msg
-	lea dx,msg
-	mov ah,09h
-	int 21h
-endm
-input macro var
-	mov ah,01h
-	int 21h
-	sub al,30h
-	mov var,al
-endm
+	list db 01h,02h,03h,04h,05h
+	msg db ' enter the number : $'
+	str db ' average : $'
+	display macro msg
+		lea dx,msg
+		mov ah,09h
+		int 21h
+	endm
+	input macro var
+		mov ah,01h
+		int 21h
+		sub al,30h
+		mov var,al
+	endm
 data ends
 
 code segment
@@ -34,28 +33,16 @@ start:
 	add al,bl
 	adc al,cl
 	mov ah,00h
-	jc carry
 	mov dh,03h
 	div dh
-	jmp last
-
-carry:
-	mov dh,0003h
-	div dh
-
-last:
-	mov cl,ah
+	
 	display str
+
 	add al,30h
 	mov dl,al
 	mov ah,02h
 	int 21h
-	display str1
-	mov al,cl
-	add al,30h
-	mov dl,al
-	mov ah,02h
-	int 21h
+
 	mov ah,4ch
 	int 21h
 code ends
